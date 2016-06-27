f7c1f5824168d4d395041852599243655ecd8d07
MichaelKang <mgkang0206@gmail.com>
2016-06-27 15:46:58 -0700

    first commit

---
+++ src/Car.java
@@ -0,0 +1,8 @@
+/**
+ * Created by mgkan on 2016-06-27.
+ */
+public class Car {
+    public int Truck;
+    public int Tractor;
+    public int Bike;
+}
---
+++ src/Garage.java
@@ -0,0 +1,6 @@
+/**
+ * Created by mgkan on 2016-06-27.
+ */
+public class Garage {
+
+}
